f9c9b447a9fae012652fce88321078224f22c3c4
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-02-11 21:01:49 +0000

    Created HelloWorldStyle.swift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Spencer Scarlett
+//  Created on 2024-Feb-08
+//  Version 1.0
+//  Copyright (c) 2024 Spencer Scarlett. All rights reserved.
+//
+//  Hello world with style so cool!
+
+// Print statement, for the code!
+print("Hello world with style in the code.!")
